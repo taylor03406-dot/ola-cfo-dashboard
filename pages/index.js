75fc793b6e603193630a371509d6f097a07ed868
taylor03406-dot <taylor03406@gmail.com>
2026-05-21 12:12:16 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 
+
+
 export default function Dashboard() {
   const [expenses, setExpenses] = useState([]);
   const [loading, setLoading] = useState(true);
